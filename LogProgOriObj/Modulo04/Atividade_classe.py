31496176a46a0fb61c614d60a3be3907961beef9
Cleber de Almeida <65259666+clebits@users.noreply.github.com>
2022-09-11 00:26:00 -0300

    atualizacao

--- LogProgOriObj/Modulo04/Atividade_classe.py
+++ LogProgOriObj/Modulo04/Atividade_classe.py
@@ -1,4 +1,4 @@
-class televisao:
+class Televisao:
     def __init__(self, marca, polegadas, preço):
         self.marca = marca
         self.polegadas = polegadas
@@ -11,12 +11,12 @@ class televisao:
     def ligar_televisao(self):
         print('Televisão ligada!')
 
-televisao.ligar_televisao(True)
-televisao1 = televisao('Samsung', '85 polegadas', '90.000 Reais')
+Televisao.ligar_televisao(True)
+televisao1 = Televisao('Samsung', '85 polegadas', '90.000 Reais')
 print(televisao1.marca)
-televisao2 = televisao('Samsung', '32 polegadas', '1.288 Reais')
+televisao2 = Televisao('Samsung', '32 polegadas', '1.288 Reais')
 print(televisao2.polegadas)
-televisao3 = televisao('LG', '65 polegadas', '8.500 Reais')
+televisao3 = Televisao('LG', '65 polegadas', '8.500 Reais')
 print(televisao3.preço)
 
-televisao.mudar_volume(televisao1,'+')
+Televisao.mudar_volume(televisao1,'+')
