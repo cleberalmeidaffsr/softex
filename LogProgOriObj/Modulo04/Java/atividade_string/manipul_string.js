629bea8f06bd5f07d897c554eb7e4ec58a68ecae
Cleber de Almeida <65259666+clebits@users.noreply.github.com>
2022-09-13 13:07:16 -0300

    atividade_strings

---
+++ LogProgOriObj/Modulo04/Java/atividade_string/manipul_string.js
@@ -0,0 +1,11 @@
+let ex1 = 'cleber'
+console.log(ex1.toUpperCase())
+// transformando a palavra para maiusculo 
+
+let ex2 = '12345'
+parseInt(ex2)
+console.log(ex2)
+// transformando a palavra de caracter para inteiro
+
+console.log(ex1.length)
+// quantos caracteres tem ex1, contagem
