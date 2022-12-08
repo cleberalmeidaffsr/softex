802ef9a4da46297a39fa1e6896a08b0721e25037
Cleber de Almeida <65259666+clebits@users.noreply.github.com>
2022-12-08 15:50:26 -0300

    react na veia

--- react/primeiro-app/src/App.js
+++ react/primeiro-app/src/App.js
@@ -1,17 +1,23 @@
-import './App.css'
-import Evento from './componentes/Eventos';
-import Form from './componentes/Form';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Footer from './componentes/layout/Footer';
+import NavBar from './componentes/layout/NavBar';
+import Contato from './paginas/Contato';
+import Empresa from './paginas/Empresa';
+import Home from './paginas/Home';
 
 function App() {
 
   return (
-    <div className="App">
-      <h1>Testando Eventos</h1>
-      <Evento numero="1"/>
-      <Evento numero="2"/>
-      <Form />
-    </div>
-  );
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path='/' exact='true' element={<Home />}></Route>
+        <Route path='/contato' element={<Contato />}></Route>
+        <Route path='/empresa' element={<Empresa />}></Route>
+      </Routes>
+      <Footer/>
+    </Router>
+  )
 }
 
 export default App;
